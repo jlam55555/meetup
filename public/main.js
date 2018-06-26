f4801bb9d2b1a4894a1819a71012ea9654f40178
Jonathan Lam <jlam55555@gmail.com>
2018-06-25 23:44:04 -0400

    added basic views and socket integration

--- public/main.js
+++ public/main.js
@@ -1,4 +1,79 @@
-// connect to server
-let socket = io();
+// component to get into a room
+let RoomComponent = {
+  template: `<div id='#container'>
+  <h3>Join / Create a room</h3>
+  <input type='text' placeholder='room name' v-model='roomName' /><br>
+  <input type='text' placeholder='room key' v-model='roomKey' /><br>
+  <button @click='joinRoom'>Join Room</button><br>
+  <button @click='createRoom'>Create Room</button>
+  <div v-for='room in rooms'>{{ room }}</div>
+  <div v-if='error'>{{ error }}</div>
+</div>`,
+  props: {
+    socket: Object
+  },
+  data() {
+    return {
+      rooms: [],
+      roomName: '',
+      roomKey: '',
+      error: ''
+    };
+  },
+  methods: {
+    joinRoom() {
+      this.$emit('toggle-view');
+    },
+    createRoom() {
+      this.socket.emit('createRoom', this.roomName, this.roomKey, success => {
+        if(success) {
+          this.roomName = '';
+          this.roomKey = '';
+          this.error = '';
+          this.$emit('toggle-view');
+        } else {
+          this.error = 'Error creating room: Room name must be between 3 and 50 characters (not including leading/trailing spaces). Room key must be between 3 and 50 characters. Room names must be unique (case doesn\'t matter).';
+        }
+      });
+    }
+  },
+  // set socket.io listeners
+  created() {
+    // get initial list of rooms
+    this.socket.emit('_rooms');
 
+    // on room update, update list
+    this.socket.on('_rooms', _rooms => this.rooms = _rooms);
+  }
+};
 
+// component when in room
+let ChatComponent = {
+  template: `<div id='container'>
+  <div><button @click='leaveRoom'>Leave room</button></div>
+</div>`,
+  methods: {
+    leaveRoom() {
+      this.$emit('toggle-view');
+    }
+  }
+};
+
+// main vue.js instance
+new Vue({
+  el: '#app',
+  data: {
+    socket: io(),
+    room: false,
+  },
+  methods: {
+    toggleView() {
+      this.room = !this.room
+    }
+  },
+  computed: {
+    currentView() {
+      return this.room ? ChatComponent : RoomComponent;
+    }
+  }
+});
--- server.js
+++ server.js
@@ -4,9 +4,46 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
+// rooms (roomNames is available to the public, rooms is essentially hidden)
+let rooms = [];
+let roomNames = [];
+let Room = function(name, key) {
+  // set name and key
+  this.name = name.trim().toLowerCase();
+  this.key = key;
+
+  // check for duplicate name or invalid name/key length
+  // if invalid, return false
+  // if not, return the room
+  return (roomNames.indexOf(this.name) !== -1
+    || this.key.length < 3
+    || this.key.length > 50
+    || this.name < 3
+    || this.name.length > 50) ? false : this;
+};
+
 // socket.io
 io.on('connect', socket => {
-  
+ 
+  // rooms getter
+  socket.on('_rooms', () => socket.emit('_rooms', roomNames));
+
+  // create room
+  socket.on('createRoom', (name, key, cb) => {
+    let newRoom = Room(name, key);
+    // if new room valid, create it, join it, and alert everyone
+    if(newRoom) {
+      rooms.push(newRoom);
+      roomNames.push(newRoom.name);
+      socket.join(newRoom.name);
+      io.sockets.emit('_rooms', roomNames);
+      cb(true);
+    }
+    // or invalid return false
+    else {
+      cb(false);
+    }
+  });
 
 });
 

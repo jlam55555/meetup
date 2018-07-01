ae74dd27d39cc319ce158c5c3c3a0b7cce881813
Jonathan Lam <jlam55555@gmail.com>
2018-07-01 18:29:50 -0400

    wider range of status icons on video panes

--- public/main.js
+++ public/main.js
@@ -244,7 +244,7 @@ let ChatComponent = {
     </div>
     <div id='videos'>
       <div class='video' v-for='pcObject in pcs' @mousedown='beginDrag' title='drag video'>
-        <i :class='[ "fa-button", "fas", pcObject.isConnected ? "fa-user" : "fa-phone-volume" ]' id='video-background'></i>
+        <i :class='[ "fa-button", "fas", pcObject.isConnected ? (pcObject.hasVideo ? "fa-video" : (pcObject.hasAudio ? "fa-microphone-alt" : "fa-user")) : "fa-phone-volume" ]' id='video-background'></i>
         <video :id='"stream-" + pcObject.id' autoplay :muted.prop='pcObject.muted' v-show='pcObject.hasVideo'></video>
         <div class='video-controls'>
           <div class='video-details'>
